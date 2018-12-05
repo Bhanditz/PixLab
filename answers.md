9aae2e5ec16ba909c4c860ef269acd73a219a9c5
Connor Doane <doanecm@s.dcsdk12.org>
2018-12-05 08:41:36 -0700

    finish a2

---
+++ answers.md
@@ -0,0 +1,5 @@
+1. Pink: Red 255, Green 0, Blue 255, Alpha 255
+2. Yellow: Red 255, Green 255, Blue 0, Alpha 255
+3. Purple: Red 140, Green 0, Blue 255, Alpha 255
+4. White: Red 255, Green 255, Blue 255, Alpha 255
+5. Dark Gray: Red 255, Green 255, Blue 255, Alpha 70
--- src/ColorChooser.java
+++ src/ColorChooser.java
@@ -6,14 +6,14 @@ import java.awt.Color;
  * A class to make working with a color chooser easier for students. It uses a
  * JColorChooser to let the user pick a color and returns the chosen color
  * object.
- * 
+ *
  * @author Barb Ericson ericson@cc.gatech.edu
  */
 public class ColorChooser {
 
 	/**
 	 * Method to let the user pick a color and return the color object.
-	 * 
+	 *
 	 * @return the picked color or red if no color was picked
 	 */
 	public static Color pickAColor() {
