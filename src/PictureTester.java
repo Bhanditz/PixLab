10df61db073e6ae8d5205a308dd710d3eefd45ab
Connor Doane <doanecm@s.dcsdk12.org>
2018-12-17 21:22:29 -0700

    a7

--- src/PictureTester.java
+++ src/PictureTester.java
@@ -84,6 +84,20 @@ public class PictureTester
     temple.explore();
   }
 
+  public static void testMirrorArms() {
+    Picture snowman = new Picture("snowman.jpg");
+    snowman.explore();
+    snowman.mirrorArms();
+    snowman.explore();
+  }
+
+  public static void testMirrorGull() {
+    Picture seagull = new Picture("seagull.jpg");
+    seagull.explore();
+    seagull.mirrorGull();
+    seagull.explore();
+  }
+
   /** Method to test the collage method */
   public static void testCollage()
   {
@@ -117,10 +131,10 @@ public class PictureTester
     //testMirrorVertical();
     //testMirrorVerticalRightToLeft();
     //testMirrorHorizontal();
-    testMirrorHorizontalBotToTop();
+    //testMirrorHorizontalBotToTop();
     //testMirrorTemple();
     //testMirrorArms();
-    //testMirrorGull();
+    testMirrorGull();
     //testMirrorDiagonal();
     //testCollage();
     //testCopy();
